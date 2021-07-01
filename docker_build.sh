e3e3cccd87d8a02c05f45168c44a3bc761e68e72
TomKellyGenetics <tomkellygenetics@gmail.com>
2021-07-01 10:40:44 +0900

    update docker build

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,6 @@
 #sleep 86400
-podman build -t zumis:v2.9.7 .
-podman tag zumis:v2.9.7 tomkellygenetics/zumis:v2.9.7
-podman tag zumis:v2.9.7 tomkellygenetics/zumis:latest
-podman push tomkellygenetics/zumis:v2.9.7
-podman push tomkellygenetics/zumis:latest
+podman build -t zumis2:v2.9.7 .
+podman tag zumis2:v2.9.7 chrzie/zumis2:v2.9.7
+podman tag zumis2:v2.9.7 chrzie/zumis2:latest
+podman push chrzie/zumis2:v2.9.7
+podman push chrzie/zumis2:latest
